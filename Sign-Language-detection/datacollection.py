3d0adbd668debc0a091022a0ecee34236619e46a
Ron Anthony Sy <142371703+rnnthnysy@users.noreply.github.com>
2024-04-24 16:08:18 +0800

    Add files via upload

---
+++ Sign-Language-detection/datacollection.py
@@ -0,0 +1,53 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=2)
+offset = 20
+imgSize = 300
+counter = 0
+folder = "Data/I love you"
+
+while True:
+    success, img = cap.read()
+
+    if not success:
+        continue
+
+    hands, _ = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
+        if imgCrop.shape[0] > 0 and imgCrop.shape[1] > 0:
+            aspectRatio = h / w
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                if wCal > 0:
+                    imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                    wGap = math.ceil((imgSize - wCal) / 2)
+                    imgWhite[:, wGap: wCal + wGap] = imgResize
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                if hCal > 0:
+                    imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                    hGap = math.ceil((imgSize - hCal) / 2)
+                    imgWhite[hGap: hCal + hGap, :] = imgResize
+
+            cv2.imshow('ImageCrop', imgCrop)
+            cv2.imshow('ImageWhite', imgWhite)
+
+    cv2.imshow('Image', img)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
